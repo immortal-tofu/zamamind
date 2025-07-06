f85e8fffd07b8b5a8ec43ae3b8c95f7ef30459bd
Clément 'birdy' Danjou <clement@danjou.io>
2025-07-06 23:59:27 +0200

    feat: add footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,6 +18,17 @@ function App() {
           </div>
         </>
       </FhevmProvider>
+      <footer>
+        <p>
+          <a href="https://github.com/immortal-tofu/zamamind/">
+            Want to see the source code?
+          </a>
+        </p>
+        <p>
+          <img src="./zama.png" alt="Zama" width="16" /> Powered by{' '}
+          <a href="https://docs.zama.ai/protocol">the Zama Protocol</a>.
+        </p>
+      </footer>
     </>
   );
 }
